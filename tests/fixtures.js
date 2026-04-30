ad31194d96db879bc22ee00c44c3d59f96773bed
alexalizurchik <lizurchik.a@gmail.com>
2026-04-30 15:29:08 +0200

    fix: delete unnecessary comments

--- tests/fixtures.js
+++ tests/fixtures.js
@@ -3,12 +3,9 @@ const { TextBoxPage } = require('../pages/TextBoxPage');
 const { AlertsPage } = require('../pages/AlertsPage');
 const { FramesPage } = require('../pages/FramesPage');
 
-// Расширяем базовый тест
 const myTest = base.test.extend({
-  // Создаем фикстуру searchPage
   textBoxPage: async ({ page }, use) => {
     const textBoxPage = new TextBoxPage(page);
-    // "Передаем" готовую страницу в тест
     await use(textBoxPage);
   },
   alertsPage: async({ page }, use) => {
